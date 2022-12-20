45e2ba944017f070f606e04776f4f14939afea98
Omar Abdelwahab <omar.lebda@uaedigitallab.com>
2022-12-20 17:57:24 +0200

    Added user CRUD APIs Succefuly

--- routers/user.js
+++ routers/user.js
@@ -6,6 +6,7 @@ const multer = require('multer')
 const sharp = require('sharp')
 const router = new express.Router()
 
+// ------------------Auth------------------//
 
 //User Signup
 router.post('/users', async (req, res)=>{
@@ -58,6 +59,88 @@ router.post('/users/logoutAll', auth, async(req, res)=>{
         res.status(500).send()
     }
 })
+
+
+
+// -----------------CRUD APIs-----------------//
+
+
+// Get Profile Info
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
+});
+
+// Update Profile Info
+router.patch('/users/me', auth, async(req, res)=>{
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['firstName', 'lastName', 'location', 'about', 'email', 'password', 'age', ];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation){
+        return res.status(400).send({error: 'Invalid updates'});
+    }
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user);
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+
+//Upload avatar function
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(jpg||jpeg||png)$/)){
+            return cb(new Error('Please upload an image'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+
+//Upload Avatar API
+router.post('/users/me/avatar', auth ,upload.single('avatar'), async(req, res)=>{
+    
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next)=>{
+    res.status(400).send({error: error.message})
+})
+
+
+//Delete Avatar API
+router.delete('/users/me/avatar', auth, async(req, res) =>{
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+//Get Avatar API
+router.get('/users/:id/avatar', async(req, res) =>{
+
+    try {
+        const user = await User.findById(req.params.id)
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+        
+    } catch (error) {
+        res.status(404).send()
+    }
+})
+
+
+
 
 
 
