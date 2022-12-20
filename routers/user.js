9ea55ddd8236e258703ce91843828f8657b40f91
Omar Abdelwahab <omar.lebda@uaedigitallab.com>
2022-12-20 16:08:40 +0200

    Authenitcation and Authorization added succefuly

--- routers/user.js
+++ routers/user.js
@@ -7,7 +7,7 @@ const sharp = require('sharp')
 const router = new express.Router()
 
 
-
+//User Signup
 router.post('/users', async (req, res)=>{
 
     const user = new User(req.body);
@@ -19,5 +19,48 @@ router.post('/users', async (req, res)=>{
         res.status(400).send(error)
     }
 })
+
+
+//User Login
+router.post('/users/login', async(req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({user, token})
+    } catch (error) {
+        res.status(400).send("error")
+    }
+})
+
+//User Logout Of Current Session
+router.post('/users/logout', auth, async(req, res)=>{
+    try {
+        req.user.tokens = req.user.tokens.filter((token)=>{
+            return token.token != req.token
+        })
+
+        await req.user.save()
+        res.send()
+
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+
+//User Logout Of All Sessions
+router.post('/users/logoutAll', auth, async(req, res)=>{
+    try {
+        req.user.tokens = [];
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+
+
+
 
 module.exports = router
